5ec2b2eb8a114281138166e54f9860c2d0116ceb
Nick Garbalau <nick.g@worldviewltd.com>
2024-11-24 20:12:16 +0000

    feat: add graph of elements, draw it

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from graphviz import Digraph
 
 
 class Value:
 
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self._prev = set(_children)
         self._op = _op
+        self.label = label
 
     def __repr__(self):
         return f"Value(data={self.data})"
@@ -22,12 +24,57 @@ class Value:
         return out
 
 
-a = Value(2.0)
-b = Value(-3.0)
-c = Value(10.0)
-d = a*b + c
+a = Value(2.0, label='a')
+b = Value(-3.0, label='b')
+c = Value(10.0, label='c')
+e = a*b
+e.label = 'e'
+d = e + c
+d.label = 'd'
+f = Value(-2, label='f')
+# L gonna be output of our graph
+L = d * f
+L.label = 'L'
 
 # Graph of what is `d` and how it was produced.
-print(d)  # Value(data=4.0)
-print(d._prev)  # {Value(data=10.0), Value(data=-6.0)}
-print(d._op)  # +
+# print(d)
+# print(d._prev)
+# print(d._op)
+
+
+def trace(root):
+    # Builds a set of all nodes and edges in a graph
+    nodes, edges = set(), set()
+
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+
+def draw_dot(root):
+    dot = Digraph(format='svg', graph_attr={'rankdir': 'LR'})
+
+    nodes, edges = trace(root)
+    for n in nodes:
+        uid = str(id(n))
+        dot.node(name=uid, label="{ %s | data %.4f}" %
+                 (n.label, n.data), shape='record')
+        if n._op:
+            dot.node(name=uid + n._op, label=n._op)
+            dot.edge(uid + n._op, uid)
+
+    for n1, n2 in edges:
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+
+    # Render the graph to a file, and opens it
+    dot.render('output', view=True)
+
+    return dot
+
+
+draw_dot(L)
